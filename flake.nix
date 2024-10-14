78b17f551ce101371472104f71c7f2ae0a2b00f9
sgoudham <sgoudham@gmail.com>
2024-10-14 02:21:40 +0100

    build: update to whiskers v2

--- flake.nix
+++ flake.nix
@@ -2,19 +2,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    toolbox.url = "github:catppuccin/toolbox";
+    catppuccin-whiskers.url = "github:catppuccin/whiskers";
   };
 
   outputs = {
     self,
     flake-utils,
     nixpkgs,
-    toolbox,
+    catppuccin-whiskers,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        whiskers = inputs.toolbox.packages.${system}.whiskers;
+        whiskers = inputs.catppuccin-whiskers.packages.${system}.whiskers;
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
